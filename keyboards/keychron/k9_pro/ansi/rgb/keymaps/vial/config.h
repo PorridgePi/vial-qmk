e18b10c034cca0270a19c785aea7c8db647f820e
PorridgePi <kunbao2006@126.com>
2024-06-07 00:27:18 +0800

    feat: add Keychron K9 Pro support Following files are added by me - keyboards/keychron/k9_pro/keymaps/vial/
    Following files are copied from https://github.com/Keychron/qmk_firmware/tree/wireless_playground
    - drivers/led/snled27351*
    - keyboards/keychron/bluetooth/
    - keyboards/keychron/common/
    - keyboards/keychron/k9_pro/ (excluding keymaps/vial/)
    - quantum/rgb_matrix/
    - tmk_core/

---
+++ keyboards/keychron/k9_pro/ansi/rgb/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x0E, 0x93, 0xC3, 0x67, 0xD9, 0x9D, 0x67, 0x6A}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
